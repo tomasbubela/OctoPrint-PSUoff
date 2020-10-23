c8dde19fd7f6918a3a1fd072de663420fda2dfc2
Tomas Bubela <tomas.bubela@gmail.com>
2020-10-23 09:07:25 +0200

    Verze 1

---
+++ octoprint_psuoff/__init__.py
@@ -0,0 +1,469 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+__author__ = "Tomas Bubela"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2020 Tomas Bubela - Released under terms of the AGPLv3 License"
+
+import octoprint.plugin
+from octoprint.server import user_permission
+import time
+import subprocess
+import threading
+import os
+from flask import make_response, jsonify
+
+try:
+    from octoprint.util import ResettableTimer
+except:
+    class ResettableTimer(threading.Thread):
+        def __init__(self, interval, function, args=None, kwargs=None, on_reset=None, on_cancelled=None):
+            threading.Thread.__init__(self)
+            self._event = threading.Event()
+            self._mutex = threading.Lock()
+            self.is_reset = True
+
+            if args is None:
+                args = []
+            if kwargs is None:
+                kwargs = dict()
+
+            self.interval = interval
+            self.function = function
+            self.args = args
+            self.kwargs = kwargs
+            self.on_cancelled = on_cancelled
+            self.on_reset = on_reset
+
+
+        def run(self):
+            while self.is_reset:
+                with self._mutex:
+                    self.is_reset = False
+                self._event.wait(self.interval)
+
+            if not self._event.isSet():
+                self.function(*self.args, **self.kwargs)
+            with self._mutex:
+                self._event.set()
+
+        def cancel(self):
+            with self._mutex:
+                self._event.set()
+
+            if callable(self.on_cancelled):
+                self.on_cancelled()
+
+        def reset(self, interval=None):
+            with self._mutex:
+                if interval:
+                    self.interval = interval
+
+                self.is_reset = True
+                self._event.set()
+                self._event.clear()
+
+            if callable(self.on_reset):
+                self.on_reset()
+
+
+class PSUoff(octoprint.plugin.StartupPlugin,
+                   octoprint.plugin.TemplatePlugin,
+                   octoprint.plugin.AssetPlugin,
+                   octoprint.plugin.SettingsPlugin,
+                   octoprint.plugin.SimpleApiPlugin):
+
+    def __init__(self):
+        try:
+            global GPIO
+            import RPi.GPIO as GPIO
+            self._hasGPIO = True
+        except (ImportError, RuntimeError):
+            self._hasGPIO = False
+
+        self._pin_to_gpio_rev1 = [-1, -1, -1, 0, -1, 1, -1, 4, 14, -1, 15, 17, 18, 21, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+        self._pin_to_gpio_rev2 = [-1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+        self._pin_to_gpio_rev3 = [-1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, 5, -1, 6, 12, 13, -1, 19, 16, 26, 20, -1, 21 ]
+
+        self.GPIOMode = ''
+        self.onoffGPIOPin = 0
+        self.invertonoffGPIOPin = False
+        self.postOnDelay = 0.0
+        self.autoOn = False
+        self.enablePowerOffWarningDialog = True
+        self.powerOffWhenIdle = False
+        self.idleTimeout = 0
+        self.idleIgnoreCommands = ''
+        self._idleIgnoreCommandsArray = []
+        self.idleTimeoutWaitTemp = 0
+        self.isPSUOn = False
+        self._check_psu_state_thread = None
+        self._check_psu_state_event= threading.Event()
+        self._idleTimer = None
+        self._waitForHeaters = False
+        self._skipIdleTimer = False
+        self._configuredGPIOPins = []
+
+
+    def on_settings_initialized(self):
+        self.GPIOMode = self._settings.get(["GPIOMode"])
+        self._logger.debug("GPIOMode: %s" % self.GPIOMode)
+
+        self.onoffGPIOPin = self._settings.get_int(["onoffGPIOPin"])
+        self._logger.debug("onoffGPIOPin: %s" % self.onoffGPIOPin)
+
+        self.invertonoffGPIOPin = self._settings.get_boolean(["invertonoffGPIOPin"])
+        self._logger.debug("invertonoffGPIOPin: %s" % self.invertonoffGPIOPin)
+
+        self.postOnDelay = self._settings.get_float(["postOnDelay"])
+        self._logger.debug("postOnDelay: %s" % self.postOnDelay)
+
+        self.enablePowerOffWarningDialog = self._settings.get_boolean(["enablePowerOffWarningDialog"])
+        self._logger.debug("enablePowerOffWarningDialog: %s" % self.enablePowerOffWarningDialog)
+
+        self.powerOffWhenIdle = self._settings.get_boolean(["powerOffWhenIdle"])
+        self._logger.debug("powerOffWhenIdle: %s" % self.powerOffWhenIdle)
+
+        self.idleTimeout = self._settings.get_int(["idleTimeout"])
+        self._logger.debug("idleTimeout: %s" % self.idleTimeout)
+
+        self.idleIgnoreCommands = self._settings.get(["idleIgnoreCommands"])
+        self._idleIgnoreCommandsArray = self.idleIgnoreCommands.split(',')
+        self._logger.debug("idleIgnoreCommands: %s" % self.idleIgnoreCommands)
+
+        self.idleTimeoutWaitTemp = self._settings.get_int(["idleTimeoutWaitTemp"])
+        self._logger.debug("idleTimeoutWaitTemp: %s" % self.idleTimeoutWaitTemp)
+
+        self._configure_gpio()
+
+        self._check_psu_state_thread = threading.Thread(target=self._check_psu_state)
+        self._check_psu_state_thread.daemon = True
+        self._check_psu_state_thread.start()
+
+        self._start_idle_timer()
+
+    def _gpio_board_to_bcm(self, pin):
+        if GPIO.RPI_REVISION == 1:
+            pin_to_gpio = self._pin_to_gpio_rev1
+        elif GPIO.RPI_REVISION == 2:
+            pin_to_gpio = self._pin_to_gpio_rev2
+        else:
+            pin_to_gpio = self._pin_to_gpio_rev3
+
+        return pin_to_gpio[pin]
+
+    def _gpio_bcm_to_board(self, pin):
+        if GPIO.RPI_REVISION == 1:
+            pin_to_gpio = self._pin_to_gpio_rev1
+        elif GPIO.RPI_REVISION == 2:
+            pin_to_gpio = self._pin_to_gpio_rev2
+        else:
+            pin_to_gpio = self._pin_to_gpio_rev3
+
+        return pin_to_gpio.index(pin)
+
+    def _gpio_get_pin(self, pin):
+        if (GPIO.getmode() == GPIO.BOARD and self.GPIOMode == 'BOARD') or (GPIO.getmode() == GPIO.BCM and self.GPIOMode == 'BCM'):
+            return pin
+        elif GPIO.getmode() == GPIO.BOARD and self.GPIOMode == 'BCM':
+            return self._gpio_bcm_to_board(pin)
+        elif GPIO.getmode() == GPIO.BCM and self.GPIOMode == 'BOARD':
+            return self._gpio_board_to_bcm(pin)
+        else:
+            return 0
+
+    def _configure_gpio(self):
+        if not self._hasGPIO:
+            self._logger.error("RPi.GPIO is required.")
+            return
+        
+        self._logger.info("Running RPi.GPIO version %s" % GPIO.VERSION)
+        if GPIO.VERSION < "0.6":
+            self._logger.error("RPi.GPIO version 0.6.0 or greater required.")
+        
+        GPIO.setwarnings(False)
+
+        for pin in self._configuredGPIOPins:
+            self._logger.debug("Cleaning up pin %s" % pin)
+            try:
+                GPIO.cleanup(self._gpio_get_pin(pin))
+            except (RuntimeError, ValueError) as e:
+                self._logger.error(e)
+        self._configuredGPIOPins = []
+
+        if GPIO.getmode() is None:
+            if self.GPIOMode == 'BOARD':
+                GPIO.setmode(GPIO.BOARD)
+            elif self.GPIOMode == 'BCM':
+                GPIO.setmode(GPIO.BCM)
+            else:
+                return
+        
+        try:
+            if not self.invertonoffGPIOPin:
+                initial_pin_output=GPIO.LOW
+            else:
+                initial_pin_output=GPIO.HIGH
+            GPIO.setup(self._gpio_get_pin(self.onoffGPIOPin), GPIO.OUT, initial=initial_pin_output)
+            self._configuredGPIOPins.append(self.onoffGPIOPin)
+        except (RuntimeError, ValueError) as e:
+            self._logger.error(e)
+
+    def check_psu_state(self):
+        self._check_psu_state_event.set()
+
+    def _check_psu_state(self):
+        while True:
+            old_isPSUOn = self.isPSUOn
+
+            self.isPSUOn = self._noSensing_isPSUOn
+            
+            self._logger.debug("isPSUOn: %s" % self.isPSUOn)
+
+            if (old_isPSUOn != self.isPSUOn) and self.isPSUOn:
+                self._start_idle_timer()
+            elif (old_isPSUOn != self.isPSUOn) and not self.isPSUOn:
+                self._stop_idle_timer()
+
+            self._plugin_manager.send_plugin_message(self._identifier, dict(hasGPIO=self._hasGPIO, isPSUOn=self.isPSUOn))
+
+            self._check_psu_state_event.wait(self.sensePollingInterval)
+            self._check_psu_state_event.clear()
+
+    def _start_idle_timer(self):
+        self._stop_idle_timer()
+        
+        if self.powerOffWhenIdle and self.isPSUOn:
+            self._idleTimer = ResettableTimer(self.idleTimeout * 60, self._idle_poweroff)
+            self._idleTimer.start()
+
+    def _stop_idle_timer(self):
+        if self._idleTimer:
+            self._idleTimer.cancel()
+            self._idleTimer = None
+
+    def _reset_idle_timer(self):
+        try:
+            if self._idleTimer.is_alive():
+                self._idleTimer.reset()
+            else:
+                raise Exception()
+        except:
+            self._start_idle_timer()
+
+    def _idle_poweroff(self):
+        if not self.powerOffWhenIdle:
+            return
+        
+        if self._waitForHeaters:
+            return
+        
+        if self._printer.is_printing() or self._printer.is_paused():
+            return
+
+        self._logger.info("Idle timeout reached after %s minute(s). Turning heaters off prior to shutting off PSU." % self.idleTimeout)
+        if self._wait_for_heaters():
+            self._logger.info("Heaters below temperature.")
+            self.turn_psu_off()
+        else:
+            self._logger.info("Aborted PSU shut down due to activity.")
+
+    def _wait_for_heaters(self):
+        self._waitForHeaters = True
+        heaters = self._printer.get_current_temperatures()
+        
+        for heater, entry in heaters.items():
+            target = entry.get("target")
+            if target is None:
+                # heater doesn't exist in fw
+                continue
+
+            try:
+                temp = float(target)
+            except ValueError:
+                # not a float for some reason, skip it
+                continue
+
+            if temp != 0:
+                self._logger.info("Turning off heater: %s" % heater)
+                self._skipIdleTimer = True
+                self._printer.set_temperature(heater, 0)
+                self._skipIdleTimer = False
+            else:
+                self._logger.debug("Heater %s already off." % heater)
+
+        while True:
+            if not self._waitForHeaters:
+                return False
+            
+            heaters = self._printer.get_current_temperatures()
+            
+            highest_temp = 0
+            heaters_above_waittemp = []
+            for heater, entry in heaters.items():
+                if not heater.startswith("tool"):
+                    continue
+
+                actual = entry.get("actual")
+                if actual is None:
+                    # heater doesn't exist in fw
+                    continue
+
+                try:
+                    temp = float(actual)
+                except ValueError:
+                    # not a float for some reason, skip it
+                    continue
+
+                self._logger.debug("Heater %s = %sC" % (heater,temp))
+                if temp > self.idleTimeoutWaitTemp:
+                    heaters_above_waittemp.append(heater)
+                
+                if temp > highest_temp:
+                    highest_temp = temp
+                
+            if highest_temp <= self.idleTimeoutWaitTemp:
+                self._waitForHeaters = False
+                return True
+            
+            self._logger.info("Waiting for heaters(%s) before shutting off PSU..." % ', '.join(heaters_above_waittemp))
+            time.sleep(5)
+
+    def hook_gcode_queuing(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        skipQueuing = False
+
+        if gcode:
+            if self.powerOffWhenIdle and self.isPSUOn and not self._skipIdleTimer:
+                if not (gcode in self._idleIgnoreCommandsArray):
+                    self._waitForHeaters = False
+                    self._reset_idle_timer()
+
+            if skipQueuing:
+                return (None,)
+        
+    def turn_psu_off(self):
+        self._logger.info("Switching PSU Off")
+
+        if not self._hasGPIO:
+            return
+
+        self._logger.debug("Switching PSU Off Using GPIO: %s" % self.onoffGPIOPin)
+        if not self.invertonoffGPIOPin:
+            pin_output=GPIO.LOW
+        else:
+            pin_output=GPIO.HIGH
+
+        try:
+            GPIO.output(self._gpio_get_pin(self.onoffGPIOPin), pin_output)
+        except (RuntimeError, ValueError) as e:
+            self._logger.error(e)
+
+        if self.sensingMethod not in ('GPIO','SYSTEM'):
+            self._noSensing_isPSUOn = False
+                    
+        time.sleep(0.1)
+        self.check_psu_state()
+
+    def get_api_commands(self):
+        return dict(
+            turnPSUOff=[],
+            getPSUState=[]
+        )
+
+    def on_api_get(self, request):
+        return self.on_api_command("getPSUState", [])
+
+    def on_api_command(self, command, data):
+        if not user_permission.can():
+            return make_response("Insufficient rights", 403)
+        
+        elif command == 'turnPSUOff':
+            self.turn_psu_off()
+        elif command == 'getPSUState':
+            return jsonify(isPSUOn=self.isPSUOn)
+
+    def get_settings_defaults(self):
+        return dict(
+            GPIOMode = 'BOARD',
+            onoffGPIOPin = 0,
+            invertonoffGPIOPin = False,
+            postOnDelay = 0.0,
+            enablePowerOffWarningDialog = True,
+            powerOffWhenIdle = False,
+            idleTimeout = 30,
+            idleIgnoreCommands = 'M105',
+            idleTimeoutWaitTemp = 50
+        )
+
+    def on_settings_save(self, data):
+        old_GPIOMode = self.GPIOMode
+        old_onoffGPIOPin = self.onoffGPIOPin
+
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        
+        self.GPIOMode = self._settings.get(["GPIOMode"])
+        self.onoffGPIOPin = self._settings.get_int(["onoffGPIOPin"])
+        self.invertonoffGPIOPin = self._settings.get_boolean(["invertonoffGPIOPin"])
+        self.postOnDelay = self._settings.get_float(["postOnDelay"])
+        self.powerOffWhenIdle = self._settings.get_boolean(["powerOffWhenIdle"])
+        self.idleTimeout = self._settings.get_int(["idleTimeout"])
+        self.idleIgnoreCommands = self._settings.get(["idleIgnoreCommands"])
+        self.enablePowerOffWarningDialog = self._settings.get_boolean(["enablePowerOffWarningDialog"])
+        self._idleIgnoreCommandsArray = self.idleIgnoreCommands.split(',')
+        self.idleTimeoutWaitTemp = self._settings.get_int(["idleTimeoutWaitTemp"])
+
+        self._configure_gpio()
+
+        self._start_idle_timer()
+
+    def get_settings_version(self):
+        return 3
+
+    def on_settings_migrate(self, target, current=None):
+        if current is None:
+            current = 0
+
+        # zatim tu nic není
+
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=True)
+        ]
+
+    def get_assets(self):
+        return {
+            "js": ["js/psuoff.js"],
+            "less": ["less/psuoff.less"],
+            "css": ["css/psuoff.min.css"]
+
+        } 
+
+    def get_update_information(self):
+        return dict(
+            psuoff=dict(
+                displayName="PSU Off",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="tomasbubela",
+                repo="OctoPrint-PSUoff",
+                current=self._plugin_version,
+
+                # update method: pip w/ dependency links
+                pip="https://github.com/tomasbubela/OctoPrint-PSUoff/archive/{target_version}.zip"
+            )
+        )
+
+__plugin_name__ = "PSU Off"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = PSUoff()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.hook_gcode_queuing,
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
---
+++ octoprint_psuoff/static/js/psuoff.js
@@ -0,0 +1,62 @@
+$(function() {
+    function PSUOffViewModel(parameters) {
+        var self = this;
+
+        self.settingsViewModel = parameters[0]
+        self.loginState = parameters[1];
+        self.settings = undefined;
+        self.hasGPIO = ko.observable(undefined);
+        self.isPSUOn = ko.observable(undefined);
+        self.psu_indicator = $("#psuoff_indicator");
+
+        self.onBeforeBinding = function() {
+            self.settings = self.settingsViewModel.settings;
+        };
+
+        self.onStartup = function () {
+            self.isPSUOn.subscribe(function() {
+                // signalizace zapnutého stavu
+                self.psu_indicator.removeClass("off").addClass("on");
+            });
+            
+            $.ajax({
+                url: API_BASEURL + "plugin/psuoff",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "getPSUState"
+                }),
+                contentType: "application/json; charset=UTF-8"
+            }).done(function(data) {
+                self.isPSUOn(data.isPSUOn);
+            });
+        }
+
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "psuoff") {
+                return;
+            }
+
+            self.hasGPIO(data.hasGPIO);
+            self.isPSUOn(data.isPSUOn);
+        };
+
+    	self.turnPSUOff = function() {
+            $.ajax({
+                url: API_BASEURL + "plugin/psuoff",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "turnPSUOff"
+                }),
+                contentType: "application/json; charset=UTF-8"
+            })
+        };   
+    }
+
+    ADDITIONAL_VIEWMODELS.push([
+        PSUOffViewModel,
+        ["settingsViewModel", "loginStateViewModel"],
+        ["#navbar_plugin_psuoff", "#settings_plugin_psuoff"]
+    ]);
+});
